6ed1199719d6fe30e7360e89dd72d07671790443
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-03-20 15:25:17 +0100

    Add footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">2024 Store</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
