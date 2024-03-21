722f092d549b0dea5a6378ce383543067e258ee9
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-03-21 14:03:04 +0100

    Add display billboards

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import getBillboard from "@/actions/get-billboards";
+import Billboard from "../components/billboard";
+import Container from "../components/ui/container";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+const HomePage = async () => {
+  const billboard = await getBillboard("b1c7c088-ccd9-4857-af50-2b540b215896");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
+
+export const dynamic = "force dynamic";
 
 export default HomePage;
