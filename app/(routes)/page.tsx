fd0627105e96b080be8c97dc65fe4f155f589505
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-03-26 11:20:25 +0100

    Handle display individual product

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,4 @@
-import getBillboard from "@/actions/get-billboards";
+import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
 import Billboard from "../components/billboard";
 import Container from "../components/ui/container";
@@ -12,9 +12,9 @@ const HomePage = async () => {
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-      </div>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   );
--- app/components/ui/product-card.tsx
+++ app/components/ui/product-card.tsx
@@ -5,10 +5,19 @@ import Image from "next/image";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
 
 const ProductCard = ({ data }: { data: Product }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
           className="aspect-square object-cover rounded-md"
