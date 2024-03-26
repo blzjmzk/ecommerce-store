8766171cbb89ea82d9fbdbb8fde6da021ef80476
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-03-26 10:12:02 +0100

    Handle displaying products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,14 +1,20 @@
 import getBillboard from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
 import Billboard from "../components/billboard";
 import Container from "../components/ui/container";
+import ProductList from "../components/product-list";
 
 const HomePage = async () => {
   const billboard = await getBillboard("b1c7c088-ccd9-4857-af50-2b540b215896");
+  const products = await getProducts({ isFeatured: true });
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   );
